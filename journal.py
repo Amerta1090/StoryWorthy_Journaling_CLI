caac38fc6e140299c62bbb6cd58e1639b479637d
Abdul Majid Ridwan Tyastonoat...(nvm, its a bit long) <125855921+Amerta1090@users.noreply.github.com>
2025-08-05 16:22:38 +0700

    Update journal.py

--- journal.py
+++ journal.py
@@ -4,22 +4,24 @@ import datetime
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SPREADSHEET_ID = '1vDFoWgJ7dK0scz8xGdONPK-yg8z9pr5rWG9A6kry4QY'
+SPREADSHEET_ID = 'spreadsheet_id'
 RANGE_NAME = 'Sheet1!A:B'  # Kolom A = tanggal, B = catatan
 
 def authenticate():
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('/home/amerta/Documents/Journal CLI/token.json'):
+        creds = Credentials.from_authorized_user_file('/home/amerta/Documents/Journal CLI/token.json', SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('/home/amerta/Documents/Journal CLI/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
+        with open('/home/amerta/Documents/Journal CLI/token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
 
